abbd43dd0e01c8e924ad22c30b66dba5e22b7148
Abhinaya Pinreddy <abhinayapinreddy@gmail.com>
2026-07-09 13:51:37 +0530

    Clean notebook outputs

--- backend/speaker_api.py
+++ backend/speaker_api.py
@@ -3,14 +3,21 @@ import requests
 COLAB_URL = "https://halves-valuables-earwig.ngrok-free.dev/diarize"
 
 def get_speaker_transcript(audio_path):
+    print("Opening file...")
 
     with open(audio_path, "rb") as f:
+        print("Sending POST request...")
 
         response = requests.post(
             COLAB_URL,
-            files={"file": f}
+            files={"file": f},
+            timeout=60
         )
 
+        print("Response received:", response.status_code)
+
     response.raise_for_status()
+
+    print("Parsing JSON...")
 
     return response.json()["speaker_transcript"]
