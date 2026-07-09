655e5481f0f549baa6707faaf3064e452ec126f5
Abhinaya Pinreddy <abhinayapinreddy@gmail.com>
2026-07-09 15:48:15 +0530

    Remove pydub dependency

--- backend/services/duration.py
+++ backend/services/duration.py
@@ -1,12 +1,17 @@
-from pydub import AudioSegment
+import wave
+import contextlib
 
 def get_audio_duration(filepath):
+    try:
+        with contextlib.closing(wave.open(filepath, "rb")) as f:
+            frames = f.getnframes()
+            rate = f.getframerate()
+            duration = frames / float(rate)
 
-    audio = AudioSegment.from_file(filepath)
+        minutes = int(duration // 60)
+        seconds = int(duration % 60)
 
-    duration = len(audio) // 1000
+        return f"{minutes} min {seconds} sec"
 
-    minutes = duration // 60
-    seconds = duration % 60
-
-    return f"{minutes} min {seconds} sec"
+    except Exception:
+        return "Unknown"
