4762c130ea78e603f62bb589cb6a311369b42099
AbhinayaPinreddy <165926118+AbhinayaPinreddy@users.noreply.github.com>
2026-07-09 14:33:10 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "YOUR_DATABASE_URL"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
